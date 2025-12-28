4777277c5b9302b13b3218b97fb90675266f186e
Cooljeb <joseph.brou@gmail.com>
2025-12-28 23:32:31 +0100

    first commit

---
+++ src/main/java/com/joe/coffee/api/JoeCoffeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.joe.coffee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeCoffeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeCoffeeApiApplication.class, args);
+	}
+
+}
