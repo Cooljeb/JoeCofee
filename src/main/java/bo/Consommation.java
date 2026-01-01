81567c7dfb8d53d5ec716f55329692d31bb425a3
Cooljeb <joseph.brou@gmail.com>
2026-01-01 02:29:32 +0100

    ajout annotations, package BO et modifications heritage

--- src/main/java/bo/Consommation.java
+++ src/main/java/bo/Consommation.java
@@ -1,33 +1,48 @@
-package model;
+package bo;
+
+import jakarta.persistence.*;
+import lombok.Data;
 
 /**
  * Une consommation  référence exactement un café et une machine
  * un réglage du broyeur, intensité (réglage machine)
  */
+@Entity
+@Data
 public class Consommation {
 
     /**
-     * CodeConsommation
+     * id de la Consommation
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codeConsommation;
 
     /**
      * Réglage broyeur
      */
+    @Column(nullable = false)
     private byte reglageBroyeur;
 
     /**
      * Réglage intensité
      */
+    @Column(nullable = false)
     private byte reglageIntensite;
 
     /**
      * Café correspondant à la consommation
      */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_id")
+    @Column(nullable = false)
     private  Cafe cafe;
 
     /**
      * Machine correspondant à la consommation
      */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "machine_a_cafe_id")
+    @Column(nullable = false)
     private MachineACafe machineACafe;
 }
---
+++ src/main/java/bo/Distributeur.java
@@ -0,0 +1,21 @@
+package bo;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Définition du commerçant de type Distributeur
+ * Ce dernier ne fait que vendre du café
+ */
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@DiscriminatorValue("Distributeur")
+public class Distributeur extends Commercant {
+
+    private  String nomDuGroupeDeDistribution;
+
+
+}
