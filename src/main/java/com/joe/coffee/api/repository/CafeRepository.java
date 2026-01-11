d9c32513b895230e21d734b573b542411a8fde90
Cooljeb <joseph.brou@gmail.com>
2026-01-11 15:39:12 +0100

    création CafeRepository (#39)

---
+++ src/main/java/com/joe/coffee/api/repository/CafeRepository.java
@@ -0,0 +1,20 @@
+package com.joe.coffee.api.repository;
+
+import com.joe.coffee.api.entity.Cafe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * Repository de recherche d'un café
+ */
+
+@Repository
+public interface CafeRepository extends JpaRepository<Cafe, Long> {
+
+    Optional<Cafe> findByNomCafeIgnoreCase(String nom);
+
+    Optional<Cafe> findByDescriptionContainingIgnoreCase(String texte);
+
+}
