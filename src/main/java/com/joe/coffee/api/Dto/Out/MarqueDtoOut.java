700b73649bc790150c2c5913a06926b9193526bb
Cooljeb <joseph.brou@gmail.com>
2026-02-04 22:35:49 +0100

    Dto et Mappers de Marque (#52)

---
+++ src/main/java/com/joe/coffee/api/Dto/Out/MarqueDtoOut.java
@@ -0,0 +1,12 @@
+package com.joe.coffee.api.Dto.Out;
+
+/**
+ * Record correspondant au DTO de sortie d'une marque
+ * @param id
+ * @param Marque
+ */
+public record MarqueDtoOut(
+        Long id,
+        String Marque
+) {
+}
---
+++ src/main/java/com/joe/coffee/api/Mapper/MarqueMapper.java
@@ -0,0 +1,18 @@
+package com.joe.coffee.api.Mapper;
+
+import com.joe.coffee.api.Dto.In.MarqueDtoIn;
+import com.joe.coffee.api.Dto.Out.MarqueDtoOut;
+import com.joe.coffee.api.Entity.Marque;
+import org.mapstruct.MappingTarget;
+
+/**
+ * Mapper d'une marque
+ */
+public interface MarqueMapper {
+
+    Marque toEntity(MarqueDtoIn dto);
+
+    MarqueDtoOut toDto(Marque entity);
+
+    void updateEntityFromDto(MarqueDtoIn dto, @MappingTarget Marque entity);
+}
