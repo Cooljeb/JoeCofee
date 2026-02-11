3f62e7c53740ec247b2b91bbbf67a767fd0a7d89
Cooljeb <joseph.brou@gmail.com>
2026-02-12 00:50:05 +0100

    DTO de ArtisanTorrefacteur (#56)

---
+++ src/main/java/com/joe/coffee/api/Dto/In/ArtisanTorrefacteurDtoIn.java
@@ -0,0 +1,11 @@
+package com.joe.coffee.api.Dto.In;
+
+/**
+ * DTO d'entrée d'un artisan torréfacteur
+ */
+public record ArtisanTorrefacteurDtoIn(
+
+        String nom,
+        String anneeCreation
+) {
+}
