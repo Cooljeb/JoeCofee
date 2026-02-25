294ef4240800ec18c4f58cf7f1b3e57b1369c279
Cooljeb <joseph.brou@gmail.com>
2026-02-25 13:46:23 +0100

    maj DTO In artisan et distributeurs) (#72)

--- src/main/java/com/joe/coffee/api/Dto/In/ArtisanTorrefacteurDtoIn.java
+++ src/main/java/com/joe/coffee/api/Dto/In/ArtisanTorrefacteurDtoIn.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.Size;
  * DTO d'entrée d'un artisan torréfacteur
  */
 public record ArtisanTorrefacteurDtoIn(
+
         @NotBlank(message = "Le nom de l'artisan est obligatoire")
         @Size(min = 3, max = 50, message = "Le nom de l'artisan doit contenir entre 3 et 50 caractères")
         @Schema(
@@ -19,6 +20,46 @@ public record ArtisanTorrefacteurDtoIn(
                 maxLength = 50
         )
         String nom,
+
+        @NotBlank(message = "L'adresse de l'artisan est obligatoire")
+        @Size(min = 3, max = 255, message = "L'adresse de l'artisan doit contenir entre 3 et 255 caractères")
+        @Schema(
+                description = "Adresse de l'artisan",
+                example = "5 place du Marché, Lyon",
+                minLength = 3,
+                maxLength = 255
+        )
+        String adresse,
+
+        @NotBlank(message = "L'email de l'artisan est obligatoire")
+        @Size(min = 3, max = 80, message = "L'email de l'artisan doit contenir entre 3 et 80 caractères")
+        @Schema(
+                description = "email de l'artisan",
+                example = "artisan@lyon-cafe.fr",
+                minLength = 3,
+                maxLength = 80
+        )
+        String email,
+
+        @NotBlank(message = "Le téléphone de l'artisan est obligatoire")
+        @Size(min = 3, max = 30, message = "Le téléphone de l'artisan doit contenir entre 3 et 30 caractères")
+        @Schema(
+                description = "téléphone de l'artisan",
+                example = "0405060708",
+                minLength = 3,
+                maxLength = 30
+        )
+        String telephone,
+
+        @NotBlank(message = "Le site internet de l'artisan est obligatoire")
+        @Size(min = 3, max = 30, message = "Le site internet de l'artisan doit contenir entre 3 et 30 caractères")
+        @Schema(
+                description = "site internet de l'artisan",
+                example = "https://www.lyon-cafe.fr",
+                minLength = 3,
+                maxLength = 30
+        )
+        String siteInternet,
 
         @YearConstraint(min = 1800)
         @Schema(description = "Année de création de l'artisan", example = "2005")
