6193524637ef684200251e5fe048aeb7b860abb9
Cooljeb <joseph.brou@gmail.com>
2026-02-25 15:38:30 +0100

    maj DTo's inet out des hérités et non controllers (#76)

--- src/main/java/com/joe/coffee/api/Dto/Out/ArtisanTorrefacteurDtoOut.java
+++ src/main/java/com/joe/coffee/api/Dto/Out/ArtisanTorrefacteurDtoOut.java
@@ -11,8 +11,20 @@ public record ArtisanTorrefacteurDtoOut(
         @Schema(description = "Identifiant unique d'un AT'", example = "2")
         Long id,
 
-        @Schema(description = "Nom de l'AT'", example = "Lyon Torréfaction")
+        @Schema(description = "Nom de l'Artisan", example = "Lyon Torréfaction")
         String nom,
+
+        @Schema(description = "Adresse de l'artisan", example = "5 place du Marché, Lyon")
+        String adresse,
+
+        @Schema(description = "Email de l'artisan", example = "artisan@lyon-cafe.fr")
+        String email,
+
+        @Schema(description = "Téléphone de l'artisan", example = "0405060708")
+        String telephone,
+
+        @Schema(description = "Site internet de l'artisan", example = "https://www.lyon-cafe.fr")
+        String siteInternet,
 
         @Schema(description = "Année de création de l'artisan", example = "2005")
         String anneeCreation
