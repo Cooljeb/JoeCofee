0dd52f3312ec7bfb36d4fa646d011d08233b12ee
Cooljeb <joseph.brou@gmail.com>
2026-09-06 14:05:17 +0200

    fix(#33): map Consommation to output DTO

--- src/main/java/com/joe/coffee/api/Mapper/ConsommationMapper.java
+++ src/main/java/com/joe/coffee/api/Mapper/ConsommationMapper.java
@@ -1,24 +1,24 @@
 package com.joe.coffee.api.Mapper;
 
 import com.joe.coffee.api.Dto.In.ConsommationDtoIn;
+import com.joe.coffee.api.Dto.Out.ConsommationDtoOut;
 import com.joe.coffee.api.Entity.Consommation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-
 /**
- * Mapper d'une consommation
+ * Mapper d'une consommation.
  */
 @Mapper(componentModel = "spring")
 public interface ConsommationMapper {
 
     @Mapping(source = "cafe.id", target = "cafeId")
     @Mapping(source = "machineACafe.id", target = "machineACafeId")
-    ConsommationDtoIn toDto(Consommation entity);
+    ConsommationDtoOut toDto(Consommation entity);
 
-    // Pas de toEntity direct, on gère l'ID dans le service
-    //donc pas de toEntity directement
-
+    @Mapping(target = "codeConsommation", ignore = true)
+    @Mapping(target = "cafe", ignore = true)
+    @Mapping(target = "machineACafe", ignore = true)
     void updateEntityFromDto(ConsommationDtoIn dto, @MappingTarget Consommation entity);
 }
