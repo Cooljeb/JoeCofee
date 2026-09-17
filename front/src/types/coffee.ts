4bfe65bdc81da9c746061cb70e5e834ec18eb1bf
Cooljeb <joseph.brou@gmail.com>
2026-09-17 14:18:58 +0200

    feat(front): amorcer services API et types domaine (#107) (#113)
    * feat(front): ajouter service API cafés (#107)
    
    * feat(front): ajouter type café (#107)

---
+++ front/src/types/coffee.ts
@@ -0,0 +1,8 @@
+/**
+ * Type volontairement minimal pour amorcer la couche API.
+ * Il sera enrichi uniquement après vérification des DTO Java de Develop :
+ * TypeScript doit refléter le contrat backend et non inventer le modèle pour l'UI.
+ */
+export interface Coffee {
+  codeCafe: number
+}
